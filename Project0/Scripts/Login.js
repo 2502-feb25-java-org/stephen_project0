776df39f95ffe89741e8e4fe25b1f44057764abb
archaether <archaether@gmail.com>
2019-03-10 23:27:52 -0400

    Added a Login

--- Project0/Scripts/Login.js
+++ Project0/Scripts/Login.js
@@ -1,30 +1,43 @@
-function saveRemember(){
+function saveRemember() {
     debugger;
-    if(document.getElementById('remember').checked==true){
-        localStorage.accountname=document.getElementById('accountname').value
-        localStorage.remember=document.getElementById('remember').value
+    if (document.getElementById('remember').checked == true) {
+        localStorage.accountname = document.getElementById('accountname').value
+        localStorage.remember = document.getElementById('remember').value
         console.log('saved');
     }
     else {
-        localStorage.remember="off";
+        localStorage.remember = "off";
         console.log('not Saved');
     }
 }
 
-function loadRemember(){
-    if(localStorage.remember =='on'){
-        document.getElementById('accountname').value=localStorage.accountname
+function loadRemember() {
+    if (localStorage.remember == 'on') {
+        document.getElementById('accountname').value = localStorage.accountname
         document.getElementById('remember').checked = true;
         console.log('loaded');
     }
 }
 
-function validate(){
-    if(document.getElementById('accountname').value ==""){
+function validate() {
+    if (document.getElementById('accountname').value == "") {
         alert("Please enter your username!");
     }
-    else if(document.getElementById('password').value==""){
+    else if (document.getElementById('password').value == "") {
         alert('Please put in your password!');
     }
-    
+    else{
+        localStorage.username = document.getElementById('accountname').value;
+        localStorage.password = document.getElementById('password').value;
+        location.reload();
+    }
+
+}
+
+
+function checkUser() {
+    if (localStorage.username == "admin" && localStorage.password == "Password123") {
+        document.getElementById('gameLink').style.display = 'block';
+        
+    }
 }
