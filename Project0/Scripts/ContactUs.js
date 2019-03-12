8aaeea2eb47cdf63147d3d23422fe596f6a2028e
archaether <archaether@gmail.com>
2019-03-12 12:04:57 -0400

    Cleaned up LoadChanges() function to prevent undefined values

--- Project0/Scripts/ContactUs.js
+++ Project0/Scripts/ContactUs.js
@@ -1,28 +1,35 @@
-function saveChanges(){
-    localStorage.name=document.getElementById('name').value  
-    localStorage.contactEmail=document.getElementById('email').value 
-    localStorage.query=document.getElementById('query').value
+function saveChanges() {
+    localStorage.name = document.getElementById('name').value
+    localStorage.contactEmail = document.getElementById('email').value
+    localStorage.query = document.getElementById('query').value
     console.log('saved')
 }
 
-function loadChanges(){
-    document.getElementById('name').value=localStorage.name 
-    document.getElementById('email').value=localStorage.contactEmail
-    document.getElementById('query').value=localStorage.query
+function loadChanges() {
+    if (localStorage.name != undefined) {
+        document.getElementById('name').value = localStorage.name
+    }
+    if (localStorage.contactEmail != undefined) {
+        document.getElementById('email').value = localStorage.contactEmail
+    }
+
+    if (localStorage.query != undefined) {
+        document.getElementById('query').value = localStorage.query
+    }
     console.log('loaded')
 }
-function validate(){
-    if(document.getElementById('name').value == ""){
+function validate() {
+    if (document.getElementById('name').value == "") {
         alert("Please fill in your name!");
     }
-    else if(document.getElementById('email').value == ''){
+    else if (document.getElementById('email').value == '') {
         alert('Please fill in your email:');
     }
-    
-    else if(document.getElementById('query').value == ''){
+
+    else if (document.getElementById('query').value == '') {
         alert('Please give us your feedback:');
     }
-    else{
+    else {
         alert('Thank you for your feedback!');
     }
 }
